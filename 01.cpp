6ed00bb51c769b8b2d2a797a7ccd17b2ad72fe08
YeRunDong7 <3634387109@qq.com>
2026-03-24 21:12:41 +0800

    一个错误的思路

--- 01.cpp
+++ 01.cpp
@@ -1,75 +1,40 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class Complex {
+class HugeInteger {
 public:
-	Complex(int a = 0, int b = 0) :realPart(a), imaginaryPart(b) {}
-	friend Complex operator+(const Complex& a,const Complex& b);
-	friend Complex operator-(const Complex& a, const Complex& b);
-	friend Complex operator*(const Complex& a, const Complex& b);
-	friend Complex operator/(const Complex& a, const Complex& b);
-	friend istream& operator >>(istream& is, Complex& a);
-	friend ostream& operator<<(ostream& os, Complex& a);
-
-private:
-	double realPart;
-	double imaginaryPart;
+	void input();
+	void output();
+	int* add(HugeInteger a);
+	void subtract();
+	int* arr = new int[40];
+	int len = 0;
 };
-
-Complex operator+(const Complex& a, const Complex& b) {
-	Complex temp;
-	temp.realPart = a.realPart + b.realPart;
-	temp.imaginaryPart = a.imaginaryPart + b.imaginaryPart;
-	return temp;
-}
-Complex operator-(const Complex& a, const Complex& b) {
-	Complex temp;
-	temp.realPart = a.realPart - b.realPart;
-	temp.imaginaryPart = a.imaginaryPart - b.imaginaryPart;
-	return temp;
-}
-Complex operator*(const Complex& a, const Complex& b) {
-	Complex temp;
-	temp.realPart = a.realPart * b.realPart - a.imaginaryPart * b.imaginaryPart;
-	temp.imaginaryPart = a.realPart * b.imaginaryPart + a.imaginaryPart * b.realPart;
-	return temp;
-}
-Complex operator/(const Complex& a, const Complex& b) {
-	Complex temp;
-	double denominator = b.realPart * b.realPart + b.imaginaryPart * b.imaginaryPart;
-	temp.realPart = (a.realPart * b.realPart + a.imaginaryPart * b.imaginaryPart) / denominator;
-	temp.imaginaryPart = (a.imaginaryPart * b.realPart - a.realPart * b.imaginaryPart) / denominator;
-	return temp;
-}
-istream& operator >>(istream& is, Complex& a) {
-	string s;
-	getline(is, s);
-	int pos = s.find(',');
-	a.realPart = stod(s.substr(0, pos));
-	a.imaginaryPart = stod(s.substr(pos + 1));
-	return is;
-}
-ostream& operator<<(ostream& os, Complex& a) {
-	os << '(' << a.realPart << ',' << a.imaginaryPart << ')';
-	return os;
-}
-int main() {
-	string s, z;
-	Complex a;
-	double c, d;
-	cin >> a;
-	while (getline(cin, s)) {
-		if (s.empty()) continue;
-		int pos1 = s.find(',');
-		int pos2 = s.find(',', pos1 + 1);
-		z = s.substr(0, pos1);
-		c = stod(s.substr(pos1 + 1, pos2-pos1-1));
-		d = stod(s.substr(pos2 + 1));
-		Complex b(c, d);
-		if (z == "add") a = a + b;
-		else if (z == "sub") a = a - b;
-		else if (z == "mul") a = a * b;
-		else if (z == "div") a = a / b;
+void HugeInteger::input() {
+	int n;
+	while (cin >> n) {
+		arr[len] = n;
+		len++;
+		if(i==40)
+			break;
 	}
-	cout << a << endl;
+}
+void HugeInteger::output() {
+	for (int i = 0; i < len; i++) {
+		cout << arr[i];
+	}
+}
+int* HugeInteger::add(HugeInteger a) {
+	int result1 = (a.len > this->len) ? a.len : this->len;
+	HugeInteger big = (result1 == a.len) ? a : *this;
+	int result2 = (a.len > this->len) ? this->len : a.len;
+	HugeInteger small = (result2 == a.len) ? a : *this;
+	int* sum = new int[40];
+	for (int i = result2; i > 0; i--) {
+		sum[i]= small.arr[i] + big.arr[i];
+	}
+	for (int i = result1 - result2; i >= 0; i--) {
+		sum[i] = big.arr[i];
+	}
+
 }
